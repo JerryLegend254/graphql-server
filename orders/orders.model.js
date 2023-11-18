bf9b0e93830aa4fbc47aab895d1eff6b687e674d
JerryLegend254 <jerryokuto713@gmail.com>
2023-11-18 14:17:00 +0300

    first commit

---
+++ orders/orders.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    date: "2023-09-10",
+    items: [
+      {
+        product: { id: "bluejean", description: "Blue Jean", price: 1000 },
+        quantity: 12,
+      },
+    ],
+    subtotal: 12000,
+  },
+];
---
+++ products/products.model.js
@@ -0,0 +1,18 @@
+module.exports = [
+  { id: "redshoe", description: "Red Shoe", price: 2000 },
+  {
+    id: "bluejean",
+    description: "Blue Jean",
+    price: 1000,
+    reviews: [{ rating: 3 }],
+  },
+  {
+    id: "tie",
+    description: "Tie",
+    price: 200,
+    reviews: [
+      { rating: 4, comment: "Amazing tie" },
+      { rating: 5, comment: "The best tie of it's price" },
+    ],
+  },
+];
